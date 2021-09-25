9b83529700d5f2c84d1a05ef1bf2d4df9a4203f3
galih9 <galihbolang07@gmail.com>
2021-09-26 02:02:48 +0700

    add 2d physics

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		var grassEffect = load("res://Effects/GrassEffect.tscn").instance()
-		grassEffect.global_position = global_position
-		get_tree().current_scene.add_child(grassEffect)
-		queue_free()
+func create_grass_effect():
+	var grassEffect = load("res://Effects/GrassEffect.tscn").instance()
+	grassEffect.global_position = global_position
+	get_tree().current_scene.add_child(grassEffect)
+
+func _on_Hurtbox_area_entered(area):
+	create_grass_effect()
+	queue_free()
